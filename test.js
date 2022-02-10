a994ab6837a1d5e57b41b3ebd428b93c04f033f6
GeorgeCheng1023 <ck40292@gmail.com>
2022-02-10 10:43:47 +0800

    edit_mainPage

--- test.js
+++
@@ -1,8 +0,0 @@
-function isWhich(min, max) {
-    return function(num) {
-        return num > min && num <= max;
-    }
-}
-const isChiled = isWhich(0, 18);
-const isAdult = isWhich(18, 60);
-const isElder = isWhich(60, 100);
