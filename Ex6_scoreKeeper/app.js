55c373e58ac7a5a52d6b170f65a1f4fabb5a48b2
GeorgeCheng1023 <ck40292@gmail.com>
2022-02-15 15:12:53 +0800

    init_ex6ScoreKeeper

---
+++ Ex6_scoreKeeper/app.js
@@ -0,0 +1,51 @@
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const scoreGoalSelect = document.querySelector('#scoreGoal');
+
+
+let scoreGoal = 3;
+let p1Score = 0;
+let p2Score = 0;
+let isGameOver = false;
+p1Button.addEventListener('click', function() {
+    if (!isGameOver) {
+        p1Score++;
+        if (p1Score === scoreGoal) {
+            isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+p2Button.addEventListener('click', function() {
+    if (!isGameOver) {
+        p2Score++;
+        if (p2Score === scoreGoal) {
+            isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
+        }
+        p2Display.textContent = p2Score;
+    }
+})
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
+
+scoreGoalSelect.addEventListener('change', function() {
+    reset();
+    scoreGoal = parseInt(scoreGoalSelect.value);
+})
