093e215d2b74203d0315649991bedad0151baecb
GeorgeCheng1023 <ck40292@gmail.com>
2022-02-19 23:15:06 +0800

    edit_test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
-console.log(__dirname);
+app.get('/home', (req, res) => {
+    res.send('Response home')
+})
+app.listen(8080, () => {
+    console.log("listening on port 8080");
+});
