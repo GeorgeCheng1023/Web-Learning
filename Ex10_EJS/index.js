1e6642f4c1724cc2cbfc95f5e0e1cfc8c0dc4a16
GeorgeCheng1023 <ck40292@gmail.com>
2022-02-20 16:19:53 +0800

    edit_ex10_addNavBar

--- Ex10_EJS/index.js
+++ Ex10_EJS/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var data = require('./data.json');
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+const redditData = require('./data.json');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/:input', (req, res) => {
-    var { input } = req.params;
-    var inputData = data[input];
-    res.render('home', {...inputData });
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', {...data });
+    } else {
+        res.render('notfound', { subreddit })
+    }
+})
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
-});
+    console.log("LISTENING ON PORT 3000")
+})
