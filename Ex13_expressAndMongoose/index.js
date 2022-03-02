b912e22ad74106f90ebd6e1c08fab6a61dfe45b9
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-02 14:45:37 +0800

    init_ex13_installMongoose

--- Ex13_expressAndMongoose/index.js
+++ Ex13_expressAndMongoose/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+    console.log('mongoose connected');
+}
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
