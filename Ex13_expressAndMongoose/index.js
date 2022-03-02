e47ffc398eef54081c40a45bb44ab70155741356
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-02 14:36:42 +0800

    init_ex13

---
+++ Ex13_expressAndMongoose/index.js
@@ -0,0 +1,15 @@
+const { log } = require('console');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.send('test');
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
