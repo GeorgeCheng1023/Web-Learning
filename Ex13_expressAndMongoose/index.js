b7dd6a9d442287fce5158013da11bf99631b8966
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-04 12:48:37 +0800

    edit_ex13

--- Ex13_expressAndMongoose/index.js
+++ Ex13_expressAndMongoose/index.js
@@ -2,8 +2,9 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const Product = require('./models/product');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 main().catch(err => console.log(err));
 
@@ -12,13 +13,73 @@ async function main() {
     console.log('mongoose connected');
 }
 
+const category_list = ['fruit', 'vegetable', 'diary']
+
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
-    res.send('test');
+    res.send('Main page');
+});
+
+//Create - to new page
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { category_list })
+});
+
+//Create - post
+app.post('/products', async(req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+})
+
+//Read - all or categories
+app.get('/products', async(req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/product.ejs', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/product.ejs', { products, category: 'All' });
+    }
+})
+
+//Read - by id
+app.get('/products/:id', async(req, res) => {
+    const { id } = req.params;
+    const foundProduct = await Product.findById(id);
+    res.render('products/show', { product: foundProduct });
+})
+
+//Update - to edit page
+app.get('/products/:id/edit', async(req, res) => {
+    const { id } = req.params;
+    const foundProduct = await Product.findById(id);
+    res.render('products/edit', { product: foundProduct, category_list });
+})
+
+//Update - rewrite products
+app.put(('/products/:id'), async(req, res) => {
+    const { id } = req.params;
+    const updateProduct = req.body;
+    console.log(updateProduct);
+    await Product.findByIdAndUpdate(id, updateProduct, { runValidators: true });
+    res.redirect(`/products/${id}`);
+})
+
+//DELETE
+app.delete('/products/:id', async(req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
 })
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
-})
+});
---
+++ Ex13_expressAndMongoose/seeds/seeds.js
@@ -0,0 +1,34 @@
+const { log } = require('console');
+const mongoose = require('mongoose');
+const Product = require('./models/product.js');
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+    console.log('mongoose connected');
+}
+
+const productList = [{
+        name: 'Apple',
+        price: 30,
+        category: 'fruit'
+    },
+    {
+        name: 'Orange',
+        price: 40,
+        category: 'fruit'
+    },
+    {
+        name: 'Patato',
+        price: 10,
+        category: 'vegetable'
+    }
+];
+
+Product.insertMany(productList)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
