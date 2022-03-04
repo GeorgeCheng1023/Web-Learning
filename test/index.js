e0fee771f075cce39a3f2b8bae8c5621560e2336
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-04 12:49:20 +0800

    delete_test

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/home', (req, res) => {
-    res.send('Response home')
-})
-app.listen(8080, () => {
-    console.log("listening on port 8080");
-});
