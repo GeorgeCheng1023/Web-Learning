6dee7a42b38ccac7ff091c59db0de7ff51f0264e
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-18 13:01:39 +0800

    splitReviewRouter

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,13 +4,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const { campgroundSchema, reviewSchema } = require('./models/Schema')
-const Review = require('./models/review')
-const Campground = require('./models/compground');
-const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError');
-const campgroundRoutes = require('./routes/campgroundRoutes')
-    //connet to mongo
+const campgroundRouter = require('./routes/campgroundRouter')
+const reviewRouter = require('./routes/reviewRouter');
+
+//connet to mongo
 main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/yelpcamp');
@@ -26,53 +24,20 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds', campgroundRouter);
+app.use('/campgrounds/:id/reviews/', reviewRouter)
 
-//Error Handle - joi schema check review
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const message = error.details.map(errDetails => errDetails.message).join(', ');
-        throw new ExpressError(message, 400);
-    } else {
-        next();
-    }
-};
-//Create review - get to reviews list
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
-    const { id } = req.params;
-    const foundCampground = await Campground.findById(id);
-    const newReview = new Review(req.body.review);
-    foundCampground.reviews.push(newReview);
-    await foundCampground.save();
-    await newReview.save();
-    res.redirect(`/campgrounds/${id}`);
-}))
+// //Error handler - page not found
+// app.all('*', (req, res, next) => {
+//     next(new ExpressError('Page Not Found', 404))
+// })
 
-//Delete review 
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params;
-    const foundCampground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    const deleteData = await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
-
-
-
-
-
-
-//Error handler - page not found
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
-
-//Error handler - undefind
-app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err
-    if (!err.message) err.message = 'Something went wrong'
-    res.status(statusCode).render('error', { err });
-})
+// //Error handler - undefind
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500 } = err
+//     if (!err.message) err.message = 'Something went wrong'
+//     res.status(statusCode).render('error', { err });
+// })
 
 app.listen(3000, () => {
     console.log("listening on port 3000");
--- YelpCamp/routes/reviewRouter.js
+++ YelpCamp/routes/reviewRouter.js
@@ -1,34 +1,38 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync')
 const Campground = require('../models/compground');
+const Review = require('../models/review')
 const { reviewSchema } = require('../models/Schema');
 
 //Error Handle - joi schema check review
 const validateReview = (req, res, next) => {
-        const { error } = reviewSchema.validate(req.body);
-        if (error) {
-            const message = error.details.map(errDetails => errDetails.message).join(', ');
-            throw new ExpressError(message, 400);
-        } else {
-            next();
-        }
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(errDetails => errDetails.message).join(', ');
+        throw new ExpressError(message, 400);
+    } else {
+        next();
     }
-    //Create review - get to reviews list
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
+};
+//Create review - get to reviews list
+router.post('/', validateReview, catchAsync(async(req, res) => {
     const { id } = req.params;
+    console.log(req.params.id);
     const foundCampground = await Campground.findById(id);
     const newReview = new Review(req.body.review);
     foundCampground.reviews.push(newReview);
     await foundCampground.save();
     await newReview.save();
     res.redirect(`/campgrounds/${id}`);
-}))
+}));
 
 //Delete review 
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
+router.delete('/:reviewId', catchAsync(async(req, res) => {
     const { id, reviewId } = req.params;
     const foundCampground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     const deleteData = await Review.findByIdAndDelete(reviewId);
     res.redirect(`/campgrounds/${id}`);
-}))
+}));
+
+module.exports = router;
