23ef56d46f9b0a7bd1f3db23a41043d55f4e3ca7
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-20 14:54:39 +0800

    addLogin

--- YelpCamp/routes/userRouter.js
+++ YelpCamp/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
 
 //get to regist page
 router.get('/register', (req, res, next) => {
@@ -9,8 +10,9 @@ router.get('/register', (req, res, next) => {
 
 // post regist 
 router.post('/', async(req, res, next) => {
+    console.log(req.body)
     try {
-        const { email, password, username } = req.body.user;
+        const { email, password, username } = req.body;
         const user = new User({ email, username });
         const registUser = await User.register(user, password);
         req.flash('success', 'You have successfully registered')
@@ -19,6 +21,17 @@ router.post('/', async(req, res, next) => {
         req.flash('error', 'Username or email already registered')
         res.redirect('/users/register');
     }
+})
+
+//get to login page
+router.get('/login', async(req, res) => {
+    res.render('users/login')
+})
+
+//post login authentication
+router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: true }), async(req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/');
 })
 
 module.exports = router;
