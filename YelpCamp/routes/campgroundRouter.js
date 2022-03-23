d2cf75e728e07449c8f4c2b3d09d1dd123a07d58
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-23 11:56:50 +0800

    format campground router

--- YelpCamp/routes/campgroundRouter.js
+++ YelpCamp/routes/campgroundRouter.js
@@ -1,37 +1,47 @@
 const express = require('express');
-const router = express.Router({
-    mergeParams: true
-});
+const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync')
 
-const {
-    validateCampground
-} = require('../utils/validate');
+const { validateCampground } = require('../utils/validate');
 const isLoggedIn = require('../utils/isLoggIn');
-const {
-    isCampgroundAuthor
-} = require('../utils/isAuthor');
+const { isCampgroundAuthor } = require('../utils/isAuthor');
 const campgroundController = require('../controllers/campgroundController');
 
-
 //Read  - all
-router.get('/', campgroundController.index);
+router.get('/',
+    campgroundController.index);
 
 //Create a new
-router.get('/new', isLoggedIn, campgroundController.toNew);
+router.get('/new',
+    isLoggedIn,
+    campgroundController.toNew);
 //Read - id
-router.get('/:id', catchAsync(campgroundController.showById));
+router.get('/:id',
+    catchAsync(campgroundController.showById));
 
 //Create - post request
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgroundController.new));
+router.post('/',
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgroundController.new));
 
 //Update - to edit
-router.get('/:id/edit', isLoggedIn, isCampgroundAuthor, catchAsync(campgroundController.toUpdate));
+router.get('/:id/edit',
+    isLoggedIn,
+    isCampgroundAuthor,
+    catchAsync(campgroundController.toUpdate));
 
 //Update - put request
-router.put('/:id', isLoggedIn, isCampgroundAuthor, validateCampground, catchAsync(campgroundController.update));
+router.put('/:id',
+    isLoggedIn,
+    isCampgroundAuthor,
+    validateCampground,
+    catchAsync(campgroundController.update));
 
 //Delete - delete request
-router.delete('/:id', isLoggedIn, isCampgroundAuthor, catchAsync(campgroundController.delete))
+router.delete('/:id',
+    isLoggedIn,
+    isCampgroundAuthor,
+    catchAsync(campgroundController.delete))
 
 module.exports = router;
