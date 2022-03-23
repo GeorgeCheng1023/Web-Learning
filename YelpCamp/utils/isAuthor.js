cbb70cd86bee23d7eab765c3b458e791411fefdd
GeorgeCheng1023 <ck40292@gmail.com>
2022-03-23 09:37:23 +0800

    fix isAuthor

--- YelpCamp/utils/isAuthor.js
+++ YelpCamp/utils/isAuthor.js
@@ -1,11 +1,10 @@
 const Campground = require('../models/compground');
 module.exports = async(req, res, next) => {
     const { id } = req.params;
-    const redirectUrl = req.session.returnTo;
     const foundCampground = await Campground.findById(id);
     if (!foundCampground.author.equals(req.user._id)) {
         req.flash('error', 'Sorry you are not allowed to do this.')
-        res.redirect(redirectUrl);
+        res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
